11428c69afd98b51ca72206ac65527cc8845c0cb
Will McAllister <willmca32@gmail.com>
2019-09-26 09:48:58 -0400

    React app ready to build

---
+++ nba-frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Route, Link, Switch } from "react-router-dom"
+import styled, { ThemeProvider } from "styled-components"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      YO
+      
+    </div>
+  );
+}
+
+export default App;
